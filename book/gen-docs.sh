81a88f6907fd6ad7f06dca48f802730eb2a6ddd2
Simon Janes <simon.janes@ibm.com>
2020-11-26 08:11:15 -0600

    infra: Customize gh-pages generator for this fork

--- book/gen-docs.sh
+++ book/gen-docs.sh
@@ -3,14 +3,17 @@
 # This file is used to generate the Proptest Book and deposit it in the
 # appropriate place to be hosted on GH pages.
 #
-# Note that it uses absolute paths.
+# Note that it uses a repository reference to a fork of the altsysrq repo.
 
 set -eux
 
 mdbook build
-cd ~/p/misc/altsysrq.github.io/
-git rm -rf proptest-book
-rm -rf proptest-book
-cp -a ~/p/rs/proptest/book/book proptest-book
-git add proptest-book
+git clone git@github.com:sj4nes/proptest.git -b gh-pages staging
+cd staging
+git rm -rf *
+cp -r ../book/* .
+git add *
 git commit -qm 'Update proptest-book.'
+git push
+cd ..
+rm -rf staging
